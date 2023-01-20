4f8d58cd76fde35f540033ec4eb7b9fd910f96a0
Victormoroo <victorhugo0609@outlook.com>
2023-01-20 03:20:01 -0300

    add: functions 'add' and 'save'

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,12 +1,27 @@
 const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    run: ['01-01', '01-05', '01-07'],
-    gym: ['01-03', '01-08'],
-    water: ['01-01', '01-02', '01-03', '01-04'],
-    study: ['01-02', '01-02', '01-04', '01-06']
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5) //biblioteca do próprio js para data no momento atual
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+        alert('Dia já está incluso nas suas anotações! ❌')
+        return
+    }
+
+    nlwSetup.addDay(today)
 }
 
+function save() {
+    localStorage.setItem('NLWSetup@memory', JSON.stringify(nlwSetup.data))
+    //localStorage - guarda as informações dentro do navegador
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@memory')) || {} //|| = ou se o objeto estiver vazio ele vai atribuir
 nlwSetup.setData(data)
 nlwSetup.load()
